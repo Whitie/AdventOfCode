944b84a1ca0ef095b85e859f0d8a37c25e035ea0
Whitie <weimann.th@yahoo.com>
2019-12-14 18:07:05 +0100

    Day 13 part 2 solved.

---
+++ 2019/Day_13/day13p2.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import os
+import sys
+
+from collections import defaultdict
+from operator import itemgetter
+
+sys.path.insert(0, os.path.abspath('..'))
+
+from intcode import IntcodeComputer  # noqa: E402
+
+
+INPUT_FILE = 'input.txt'
+TILES = [' ', '|', '#', '-', 'O']
+
+
+def chunks(iterable, size):
+    it = list(iterable)
+    for i in range(0, len(it), size):
+        yield it[i:i+size]
+
+
+def main(filename):
+    ic = IntcodeComputer.from_file(filename)
+    offset = 639
+    height = ic.code[604]
+    width = max(ic.code[620], ic.code[621]) // height
+    no = ic.code[632]
+    a = max(ic.code[612], ic.code[613])
+    b = max(ic.code[616], ic.code[617])
+    s = 0
+    for y in range(height):
+        for x in range(width):
+            if ic.code[offset+y*width+x] == 2:
+                s += ic.code[no+(((x*height+y)*a+b) % (width*height))]
+    print(s)
+
+
+if __name__ == '__main__':
+    try:
+        main(sys.argv[1])
+    except IndexError:
+        print(f'Usage: {sys.argv[0]} <filename>')
