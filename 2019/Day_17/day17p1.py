6532815c81108e537232bf9ea28f2199405586f6
Whitie <weimann.th@yahoo.com>
2019-12-19 20:38:10 +0100

    Day 17 part 1 solved.

--- 2019/Day_17/day17p1.py
+++ 2019/Day_17/day17p1.py
@@ -9,8 +9,39 @@ sys.path.insert(0, os.path.abspath('..'))
 from intcode import IntcodeComputer  # noqa: E402
 
 
+def is_intersection(x, y, field):
+    positions = ((x, y-1), (x-1, y), (x+1, y), (x, y+1))
+    for x, y in positions:
+        if x < 1 or y < 1:
+            return False
+        try:
+            if field[y][x] != '#':
+                return False
+        except IndexError:
+            return False
+    return True
+
+
 def main(filename):
     ic = IntcodeComputer.from_file(filename)
+    ic.run()
+    field = []
+    line = []
+    for i in ic.get_output():
+        if i == 10:
+            field.append(line[:])
+            line = []
+        else:
+            line.append(chr(i))
+    for i, line in enumerate(field):
+        print(f'{i:2d}', ''.join(line))
+    align = []
+    for y, row in enumerate(field):
+        for x, cell in enumerate(row):
+            if cell == '#' and is_intersection(x, y, field):
+                print(f'FOUND: X: {x}, Y: {y}')
+                align.append(x * y)
+    print(sum(align))
 
 
 if __name__ == '__main__':
