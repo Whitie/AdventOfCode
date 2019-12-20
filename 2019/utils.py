61787e80ea28bc4cf1474c9d41d39cdf80774519
Thorsten Weimann <weimann.th@yahoo.com>
2019-12-20 07:27:53 +0100

    Added utils.

---
+++ 2019/utils.py
@@ -0,0 +1,42 @@
+# -*- coding: utf-8 -*-
+
+
+class Point:
+
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+    @property
+    def neighbors(self):
+        return (self.left, self.right, self.up, self.down)
+
+    @property
+    def left(self):
+        return Point(self.x - 1, self.y)
+
+    @property
+    def right(self):
+        return Point(self.x + 1, self.y)
+
+    @property
+    def up(self):
+        return Point(self.x, self.y - 1)
+
+    @property
+    def down(self):
+        return Point(self.x, self.y + 1)
+
+    def __getitem__(self, index):
+        if isinstance(index, int):
+            return (self.x, self.y)[index]
+        return getattr(self, index)
+
+    def __hash__(self):
+        return hash((self.x, self.y))
+
+    def __eq__(self, other):
+        return hash(self) == hash(other)
+
+    def __add__(self, other):
+        return Point(self.x + other[0], self.y + other[1])
