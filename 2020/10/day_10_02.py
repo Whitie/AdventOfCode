c8e6c584f1c16eda561b6406a52d46da7c878d42
Whitie <weimann.th@gmail.com>
2020-12-10 11:50:46 +0100

    Day 10 part 2 solved.

--- 2020/10/day_10_02.py
+++ 2020/10/day_10_02.py
@@ -15,16 +15,32 @@ def read_input(filepath):
         return [int(line) for line in fp if line.strip()]
 
 
+def fib(n):
+    a = 0
+    b = 1
+    for _ in range(n):
+        yield a
+        a, b = b, a + b
+
+
 def main():
     jolts = read_input(INPUT)
     #jolts = read_input(TEST2)
     jolts = list(sorted(jolts))
     jolts.insert(0, 0)
-    diffs = {1: 0, 3: 1}
-    for i, jolt in enumerate(jolts[1:]):
-        diff = jolt - jolts[i]
-        diffs[diff] += 1
-    print(diffs, diffs[1] * diffs[3])
+    jolts.append(max(jolts) + 3)
+    diffs = [jolts[i] - jolts[i-1] for i in range(1, len(jolts))]
+    factors = [1] + [sum(fib(x)) for x in range(10)][2:]
+    i = 0
+    res = 1
+    while i < len(diffs):
+        in_row = 0
+        while diffs[i] == 1:
+            in_row += 1
+            i += 1
+        res *= factors[in_row]
+        i += 1
+    print(res)
 
 
 if __name__ == '__main__':
